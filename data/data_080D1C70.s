3f374d4647f321733f97cda20d4bb75d67f012a3
octorock <79596758+octorock@users.noreply.github.com>
2021-03-24 19:17:22 +0100

    WIP

--- data/data_080D1C70.s
+++ data/data_080D1C70.s
@@ -15,21 +15,3 @@
 	.include "data/animations/enemy/enemy5F.inc"
 
 	.include "data/const/enemy/gyorgChild.inc"
-	.include "data/animations/enemy/gyorgChild.inc"
-
-	.include "data/const/enemy/gyorgFemaleEye.inc"
-	.include "data/animations/enemy/gyorgFemaleEye.inc"
-
-	.include "data/const/enemy/enemy62.inc"
-	.include "data/animations/enemy/enemy62.inc"
-
-	.include "data/const/enemy/gyorgFemaleMouth.inc"
-	.include "data/animations/enemy/gyorgFemaleMouth.inc"
-
-	.include "data/const/enemy/enemy64.inc"
-
-	.include "data/const/enemy/treeItem.inc"
-
-	.include "data/const/enemy/enemy66.inc"
-
-	.include "data/const/code_08049D30.inc"
